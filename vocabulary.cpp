bfb44612d55adfe3ea09f657ff53ff7a0936fa08
Slutskiy Michael <mace_window@mail.ru>
2019-10-04 03:59:27 +0300

    Add files via upload

---
+++ vocabulary.cpp
@@ -0,0 +1,31 @@
+// Identidier and numbers
+%token VAR
+//%token NUMBER
+
+// Keywords
+%token AND OR XOR NOT IS
+%token READINT READREAL READSTRING PRINT
+%token RETURN IF THEN ELSE END WHILE FOR
+%token IN LOOP DOT
+%token REAL BOOL STRING EMPTY
+%token FUNC TRUE FALSE
+
+// Delimiters
+%token LEFTCIRCLEBRACKET		// (
+%token RIGHTCIRCLEBRACKET	//  )
+%token LEFTCURLYBRACKET		// }
+%token RIGHTCURLYBRACKET		// }
+%token  DOT					// .
+%token COMMA				// ,
+%token ARROW				// =>
+
+// Operator signs
+%token ASSIGN  				// :=  
+%token SEMICOLON		// ;
+%token LESS					// <
+%token LESSOREQUAL	// <=
+%token GREATOREQUAL	// >=
+%token EQUAL				// =
+%token DIVIDEQUAL		// /=
+%token PLUS					// +
+%token MINUS				// -
