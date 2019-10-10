737560a9eedde59c9d222e5e33c588cd43f0a567
andropov_a <andropov@geogracom.com>
2019-10-11 00:56:17 +0300

    [!]Small addition( 1=true, 0=false) + fix missing file

---
+++ src/ast/BooleanLiteral.cpp
@@ -0,0 +1,45 @@
+#include "BooleanLiteral.h"
+
+namespace AST
+{
+    BooleanLiteral::BooleanLiteral(bool val): value(val) {}
+
+    std::string BooleanLiteral::to_string()
+    {
+        return value?"true":"false";
+    }
+
+    Literal& BooleanLiteral::evaluate()
+    {
+        return *this;
+    }
+
+    Literal& BooleanLiteral::operator&&(Literal& rhs)
+    {
+        return rhs.and_operator(this);
+    }
+    Literal& BooleanLiteral::operator||(Literal& rhs)
+    {
+        return rhs.or_operator(this);
+    }
+    Literal& BooleanLiteral::operator^(Literal& rhs)
+    {
+        return rhs.xor_operator(this);
+    }
+
+    Literal& BooleanLiteral::and_operator(BooleanLiteral* rhs)
+    {
+        auto temp = new BooleanLiteral(rhs->value && this->value);
+        return *temp;
+    }
+    Literal& BooleanLiteral::or_operator(BooleanLiteral* rhs)
+    {
+        auto temp = new BooleanLiteral(rhs->value || this->value);
+        return *temp;
+    }
+    Literal& BooleanLiteral::xor_operator(BooleanLiteral* rhs)
+    {
+        auto temp = new BooleanLiteral(rhs->value ^ this->value);
+        return *temp;
+    }
+}
