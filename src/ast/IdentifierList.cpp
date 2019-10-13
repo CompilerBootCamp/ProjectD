3a1839e84687d1cb185834dc67d9a8db639b3d8d
NoJpg <dream.doomwalker@gmail.com>
2019-10-13 21:00:02 +0300

    [!] - fix identifier list

--- src/ast/IdentifierList.cpp
+++ src/ast/IdentifierList.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "IdentifierList.h"
+
+#include <utility>
+
+const std::vector<std::string> &AST::IdentifierList::getIdentifiers() const {
+    return identifiers;
+}
+
+void AST::IdentifierList::setIdentifiers(const std::vector<std::string> &new_identifiers) {
+    IdentifierList::identifiers = new_identifiers;
+}
+
+void AST::IdentifierList::addIdentifier(const std::string &identifier) {
+    this->identifiers.push_back(identifier);
+}
+
+AST::IdentifierList::IdentifierList(std::vector<std::string> identifiers) : identifiers(std::move(identifiers)) {}
--- src/ast/IdentifierList.h
+++ src/ast/IdentifierList.h
@@ -5,10 +5,26 @@
 #ifndef PROJECTD_IDENTIFIERLIST_H
 #define PROJECTD_IDENTIFIERLIST_H
 
+#include <vector>
+#include <string>
 
-class IdentifierList {
 
-};
+namespace AST{
+
+    class IdentifierList {
+    private:
+        std::vector<std::string> identifiers;
+
+    public:
+        IdentifierList(std::vector<std::string> identifiers);
+
+        [[nodiscard]] const std::vector<std::string> &getIdentifiers() const;
+
+        void setIdentifiers(const std::vector<std::string> &identifiers);
+
+        void addIdentifier(const std::string &identifier);
+    };
+}
 
 
 #endif //PROJECTD_IDENTIFIERLIST_H
