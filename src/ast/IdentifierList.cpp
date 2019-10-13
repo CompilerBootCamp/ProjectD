88e49e503627218ac462bef6b3266681a9824e61
NoJpg <dream.doomwalker@gmail.com>
2019-10-13 20:36:17 +0300

    [!] - added func lit

---
+++ src/ast/IdentifierList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sovereign on 10/13/19.
+//
+
+#include "IdentifierList.h"
---
+++ src/ast/IdentifierList.h
@@ -0,0 +1,14 @@
+//
+// Created by sovereign on 10/13/19.
+//
+
+#ifndef PROJECTD_IDENTIFIERLIST_H
+#define PROJECTD_IDENTIFIERLIST_H
+
+
+class IdentifierList {
+
+};
+
+
+#endif //PROJECTD_IDENTIFIERLIST_H
